f25eaa95446398c742c946db0b06054747fe80fc
natnaelabay <se.natnael.abay@gmail.com>
2021-09-04 16:18:27 +0300

    property_deatail bloc added

---
+++ lib/features/property/bloc/property_detail/propertydetail_state.dart
@@ -0,0 +1,23 @@
+part of 'propertydetail_bloc.dart';
+
+abstract class PropertyDetailState extends Equatable {
+  const PropertyDetailState();
+  @override
+  List<Object?> get props => [];
+}
+
+class PropertyDetailInitial extends PropertyDetailState {}
+
+class PropertyDetailLoading extends PropertyDetailState {}
+
+class PropertyDetailOperationSuccess extends PropertyDetailState {
+  final Property property;
+  const PropertyDetailOperationSuccess(this.property);
+
+  @override
+  List<Object?> get props => [
+        this.property,
+      ];
+}
+
+class PropertyDetailOperationFailure extends PropertyDetailState {}
