2af12f85535b0cc28c826313e0c9d69c3a74c147
Surafel Kassahun <se.surafel.kassahun@gmail.com>
2021-09-08 21:57:21 +0300

    Review card modified

--- lib/features/property/screens/property_detail/property_detail_screen.dart
+++ lib/features/property/screens/property_detail/property_detail_screen.dart
@@ -79,8 +79,9 @@ class PropertyDetail extends StatelessWidget {
                             widgets.add(
                               _reviewListCard(
                                 review: review.message ?? "",
-                                imageUrl: "review",
-                                username: "username",
+                                imageUrl:
+                                    "https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80",
+                                username: "Niko Altechalchm",
                               ),
                             );
                           }
